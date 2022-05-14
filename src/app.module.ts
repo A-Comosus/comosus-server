3e113485f827b5bde39fbc36c33a508847c968f0
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2022-05-14 17:47:13 +1000

    Set up `@nestjs/config` to access env vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,14 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { AppResolver } from './app.resolver';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: false, // Set ConfigModule to be accessible in other modules
+      envFilePath: ['.env', '.env.development', '.env.development.local'],
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema/schema.gql'),
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3100);
+  const config = app.get<ConfigService>(ConfigService);
+  await app.listen(parseInt(config.get('PORT'), 10) ?? 3100);
 }
 bootstrap();
