eddfd1dc82ea4ed444daa0714770c6a1fe658009
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2022-05-14 23:40:34 +1000

    Set up generated `prisma/client`

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '@prisma/client';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get<ConfigService>(ConfigService);
   await app.listen(parseInt(config.get('PORT'), 10) ?? 3100);
+
+  const prisma = new PrismaClient();
 }
 bootstrap();
