7dab51f2032b53c9f076972609e997e55bf532ff
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2022-05-15 13:17:38 +1000

    Implement `JwtAuthGuard` on user queries

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,22 +1,27 @@
-import { Resolver, Query, Args, Int } from '@nestjs/graphql';
+import { Resolver, Query, Args, Int, Context } from '@nestjs/graphql';
 import { UserService } from './user.service';
 import { User } from './entities/user.entity';
+import { JwtAuthGuard } from '@auth/guards';
+import { UseGuards } from '@nestjs/common';
 
 @Resolver(() => User)
 export class UserResolver {
   constructor(private readonly userService: UserService) {}
 
   @Query(() => [User], { name: 'users' })
+  @UseGuards(JwtAuthGuard)
   findAll() {
     return this.userService.findAll();
   }
 
   @Query(() => User, { name: 'userById' })
+  @UseGuards(JwtAuthGuard)
   findById(@Args('id', { type: () => Int }) id: number) {
     return this.userService.findById(id);
   }
 
   @Query(() => User, { name: 'userByUsername' })
+  @UseGuards(JwtAuthGuard)
   findByUsername(@Args('username') username: string) {
     return this.userService.findByUsername(username);
   }
