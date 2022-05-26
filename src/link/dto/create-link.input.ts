d87d7deac879c3047e8c7a7e22b75e70a570c06b
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2022-05-27 00:22:07 +1000

    Update create link fields to be required

--- src/link/dto/create-link.input.ts
+++ src/link/dto/create-link.input.ts
@@ -1,11 +1,17 @@
 import { InputType, Field } from '@nestjs/graphql';
+import { IsNotEmpty } from 'class-validator';
 
 @InputType()
 export class CreateLinkInput {
   @Field()
+  @IsNotEmpty()
   title: string;
+
   @Field()
+  @IsNotEmpty()
   url: string;
+
   @Field()
+  @IsNotEmpty()
   userId: string;
 }
