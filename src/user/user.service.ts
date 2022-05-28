92a6e021c020f06bf9ac6f3e5664732719507bba
Miao Zhao <miao_zhao@yahoo.com>
2022-05-29 08:21:43 +1000

    Create `createPasswordResetLink` service to save resetToken and resetTokenExpires into database, and return the reset-password link

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserInput } from './dto/create-user.input';
 import { PrismaClient } from '@prisma/client';
-
+import 'crypto';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const addHours = require('date-fns/addHours');
 @Injectable()
 export class UserService {
   prisma = new PrismaClient();
@@ -16,5 +18,24 @@ export class UserService {
 
   async findByUsername(_username: string) {
     return this.prisma.user.findFirst({ where: { username: _username } });
+  }
+
+  async createPasswordResetLink(_id: string) {
+    const { createHmac, randomBytes } = await import('crypto');
+    const secret = process.env.CRYPTO_SECRET;
+    const token = randomBytes(32).toString('hex');
+    const resetToken = createHmac('sha256', secret).update(token).digest('hex');
+    const resetTokenExpires = addHours(new Date(), 1).toISOString();
+    console.log(resetTokenExpires);
+    await this.prisma.user.update({
+      where: {
+        id: _id,
+      },
+      data: {
+        passwordResetToken: resetToken,
+        passwordResetTokenExpires: resetTokenExpires,
+      },
+    });
+    return `${process.env.CLIENT_BASE_URL}/reset-password/${resetToken}`;
   }
 }
