a19a91c08a83710b9286a7bbd0f88612920e098f
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2022-05-29 20:35:56 +1000

    Update linter to error when `console.*` is used.

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    'no-console': [2],
     '@typescript-eslint/no-unused-vars': [2],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
