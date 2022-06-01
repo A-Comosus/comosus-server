b7c596bd2dee0437a49b50844528588a996a0949
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2022-06-01 22:35:44 +1000

    Add id to `mockUserData` object

--- src/resource/user/test/mockUserData.ts
+++ src/resource/user/test/mockUserData.ts
@@ -1,15 +1,18 @@
 const mockUserData = [
   {
+    id: '0',
     email: 'test-0@example.com',
     username: 'test-0',
     password: 'secret',
   },
   {
+    id: '1',
     email: 'test-1@example.com',
     username: 'test-1',
     password: 'secret',
   },
   {
+    id: '2',
     email: 'test-2@example.com',
     username: 'test-2',
     password: 'secret',
