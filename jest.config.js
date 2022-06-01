5723718d9e6178dbe169e2e0c2cd73d9859335e1
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2022-06-01 11:08:44 +1000

    Set up `jest` configuration

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+const config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  moduleNameMapper: {
+    '^@src/(.*)$': ['<rootDir>/$1'],
+    '^@common': ['<rootDir>/common'],
+    '^@common/(.*)$': ['<rootDir>/common/$1'],
+    '^@resource': ['<rootDir>/resource'],
+    '^@resource/(.*)$': ['<rootDir>/resource/$1'],
+  },
+  collectCoverageFrom: ['<rootDir>/**/*.ts'],
+  testEnvironment: 'node',
+  verbose: true,
+};
+
+module.exports = config;
