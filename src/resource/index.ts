11a8d9ae2ad7861ac0022f177d2222289f700bd6
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2022-06-01 11:09:54 +1000

    Update index of app resources

--- src/resource/index.ts
+++ src/resource/index.ts
@@ -3,3 +3,5 @@ export { AuthModule } from './auth/auth.module';
 
 // User
 export { UserModule } from './user/user.module';
+export { UserService } from './user/user.service';
+export { User } from './user/entities/user.entity';
