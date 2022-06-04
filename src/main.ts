5d5f1276f91d4b59ce9b8b1121ed33f037b1469f
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2022-06-04 15:30:44 +1000

    Init MailingServer at server start

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ConfigService } from '@nestjs/config';
 import * as dotenv from 'dotenv';
 import { AppModule } from './resource/app/app.module';
 import { setupSwagger } from './config';
-import { PrismaService } from '@common';
+import { PrismaService, MailingService } from '@common';
 
 dotenv.config();
 
@@ -17,6 +17,9 @@ async function bootstrap() {
 
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
+
+  const mailingService = app.get(MailingService);
+  mailingService.initMailingService();
 
   await app.listen(parseInt(config.get('PORT'), 10) ?? 3100);
   Logger.log(`Server listening on ${await app.getUrl()}`);
