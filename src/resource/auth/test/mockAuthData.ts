17eba3ef44ee352cf434d4d48147ea3ba94cb0ae
Miao Zhao <miao_zhao@yahoo.com>
2022-06-07 21:35:19 +1000

    Create mockdata

---
+++ src/resource/auth/test/mockAuthData.ts
@@ -0,0 +1,31 @@
+const mockUsers = [
+  {
+    id: '0',
+    email: 'test-0@example.com',
+    username: 'test-0',
+    password: 'secret',
+    acceptPolicy: true,
+    passwordResetToken: 'abc',
+    passwordResetTokenExpires: '2023-06-04T11:41:07.790Z',
+  },
+  {
+    id: '1',
+    email: 'test-1@example.com',
+    username: 'test-1',
+    password: 'secret',
+    acceptPolicy: true,
+    passwordResetToken: '123',
+    passwordResetTokenExpires: '2022-06-04T11:56:36.238Z',
+  },
+  {
+    id: '2',
+    email: 'test-2@example.com',
+    username: 'test-2',
+    password: 'secret',
+    acceptPolicy: false,
+    passwordResetToken: '1e1',
+    passwordResetTokenExpires: '2021-06-04T11:41:07.790Z',
+  },
+];
+
+export default mockUsers;
