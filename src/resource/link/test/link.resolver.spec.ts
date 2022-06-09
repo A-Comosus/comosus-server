69c1476a43cc773cf4fb36cffcedc88fe8d02835
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2022-06-09 22:29:24 +1000

    Fix empty unit test for `LinkResolver`

--- src/resource/link/test/link.resolver.spec.ts
+++ src/resource/link/test/link.resolver.spec.ts
@@ -1,13 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { LinkResolver } from './link.resolver';
-import { LinkService } from './link.service';
+import { LinkResolver } from '../link.resolver';
+import { LinkService } from '../link.service';
 
 describe('LinkResolver', () => {
   let resolver: LinkResolver;
 
+  const mockLinkService = {};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [LinkResolver, LinkService],
+      providers: [
+        LinkResolver,
+        { provide: LinkService, useValue: mockLinkService },
+      ],
     }).compile();
 
     resolver = module.get<LinkResolver>(LinkResolver);
