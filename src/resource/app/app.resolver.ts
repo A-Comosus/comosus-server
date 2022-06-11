01583d5eb6c4088fa848afab3c3c9f31696332d3
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2022-06-11 22:01:52 +1000

    Decorate `getServerInfo` as protected

--- src/resource/app/app.resolver.ts
+++ src/resource/app/app.resolver.ts
@@ -1,7 +1,9 @@
-import { AppService } from './app.service';
+import { Logger, UseGuards } from '@nestjs/common';
 import { Query, Resolver } from '@nestjs/graphql';
+
+import { JwtAuthGuard } from '@resource/auth/guards';
 import { App } from './app.entity';
-import { Logger } from '@nestjs/common';
+import { AppService } from './app.service';
 
 @Resolver()
 export class AppResolver {
@@ -9,6 +11,7 @@ export class AppResolver {
   constructor(private readonly appService: AppService) {}
 
   @Query(() => App, { name: 'server' })
+  @UseGuards(JwtAuthGuard)
   getServerInfo() {
     this.logger.log('Receiving request for server info...');
     return this.appService.getServerInfo();
