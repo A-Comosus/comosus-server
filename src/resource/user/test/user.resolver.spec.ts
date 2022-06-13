99794e9702b5d821fc6d3127ea03d0140fb033d9
Miao Zhao <miao_zhao@yahoo.com>
2022-06-13 21:15:01 +1000

    Remove `findByResetPasswordToken`

--- src/resource/user/test/user.resolver.spec.ts
+++ src/resource/user/test/user.resolver.spec.ts
@@ -14,11 +14,6 @@ describe('UserResolver', () => {
     findByEmail: jest.fn((email) =>
       mockUserData.find((user) => user.email === email),
     ),
-    findByResetPasswordToken: jest.fn((passwordResetToken) =>
-      mockUserData.find(
-        (user) => user.passwordResetToken === passwordResetToken,
-      ),
-    ),
   };
 
   beforeEach(async () => {
@@ -48,14 +43,5 @@ describe('UserResolver', () => {
   it('should find user by email', async () => {
     const user = mockUserData[0];
     expect(resolver.findByEmail({ email: user.email })).toEqual(user);
-  });
-
-  it('should find user by resetPasswordToken', async () => {
-    const user = mockUserData[0];
-    expect(
-      resolver.findByResetPasswordToken({
-        resetPasswordToken: user.passwordResetToken,
-      }),
-    ).toEqual(user);
   });
 });
