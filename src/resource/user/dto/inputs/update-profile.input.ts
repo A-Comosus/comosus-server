d134fe76232b8bcde0568849bdf8e8650c2dfcf9
Paul XU <pyc.xuu@gmail.com>
2022-07-12 17:10:49 +1000

    Create UpdateProfileInput user dto

--- src/resource/user/dto/inputs/update-profile.input.ts
+++ src/resource/user/dto/inputs/update-profile.input.ts
@@ -2,8 +2,14 @@ import { Field, InputType } from '@nestjs/graphql';
 import { IsNotEmpty } from 'class-validator';
 
 @InputType()
-export class UpdateProfileByUsernameInput {
+export class UpdateProfileInput {
   @Field()
   @IsNotEmpty()
   username: string;
+
+  @Field()
+  displayName: string;
+
+  @Field()
+  bio: string;
 }
