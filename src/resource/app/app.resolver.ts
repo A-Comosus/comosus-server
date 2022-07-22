2d55f83c88c79079347ef6cbda2a148841cb36f1
Rita Mou <ritamxy@gmail.com>
2022-07-22 17:42:09 +1000

    create ContactFormResponse and ContactFormInput in app.resolver

--- src/resource/app/app.resolver.ts
+++ src/resource/app/app.resolver.ts
@@ -1,9 +1,10 @@
 import { Logger, UseGuards } from '@nestjs/common';
-import { Query, Resolver } from '@nestjs/graphql';
+import { Query, Resolver, Args, Mutation } from '@nestjs/graphql';
 
 import { JwtAuthGuard } from '@resource/auth/guards';
 import { App } from './app.entity';
 import { AppService } from './app.service';
+import { ContactFormResponse, ContactFormInput } from './dto';
 
 @Resolver()
 export class AppResolver {
@@ -15,5 +16,13 @@ export class AppResolver {
   getServerInfo() {
     this.logger.log('Receiving request for server info...');
     return this.appService.getServerInfo();
+  }
+
+  @Mutation(() => ContactFormResponse)
+  contactForm(@Args('detail') _contactForm: ContactFormInput) {
+    this.logger.log(
+      `Receiving request to contact us with email ${_contactForm.email}...`,
+    );
+    return this.appService.contactForm(_contactForm);
   }
 }
