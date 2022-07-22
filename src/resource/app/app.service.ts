941bee5c3aca9564bf4a608f0fe9062746460f03
Rita Mou <ritamxy@gmail.com>
2022-07-22 17:39:58 +1000

    create ContactFormInput in app.service

--- src/resource/app/app.service.ts
+++ src/resource/app/app.service.ts
@@ -1,11 +1,24 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { App } from './app.entity';
+import { ContactFormInput } from './dto';
 
 @Injectable()
 export class AppService {
   private readonly logger = new Logger(AppService.name);
+
   getServerInfo(): App {
     this.logger.log('Responding with server information...');
     return { status: 'Server is up' };
+  }
+
+  async contactForm(_contactForm: ContactFormInput) {
+    const { customerName, email, phone, message } = _contactForm;
+    this.logger.log(
+      `Receiving contact form from {customerName: ${customerName}, email: ${email}}`,
+    );
+
+    // Todo: pass customer input to database
+
+    return { status: 'success' };
   }
 }
