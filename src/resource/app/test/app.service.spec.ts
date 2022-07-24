cc912d260633172981637ca108f90938e449bee0
Miao Zhao <miao_zhao@yahoo.com>
2022-07-24 21:34:35 +1000

    Update app.service.spec.ts

--- src/resource/app/test/app.service.spec.ts
+++ src/resource/app/test/app.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppService } from '../app.service';
+import { MailingService, AxiosService } from '@src/common';
 
 describe('AppService', () => {
   let service: AppService;
 
+  const mockMailingService = {
+    sendEmail: jest.fn(),
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AppService],
+      providers: [
+        AppService,
+        { provide: MailingService, useValue: mockMailingService },
+        { provide: AxiosService, useValue: {} },
+      ],
     }).compile();
 
     service = module.get<AppService>(AppService);
