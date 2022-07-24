6e177eeda901ca6d77964dbf0e71a3182951c455
Rita Mou <ritamxy@gmail.com>
2022-07-24 20:44:22 +1000

    change Mutation response to boolean

--- src/resource/app/app.resolver.ts
+++ src/resource/app/app.resolver.ts
@@ -4,7 +4,7 @@ import { Query, Resolver, Args, Mutation } from '@nestjs/graphql';
 import { JwtAuthGuard } from '@resource/auth/guards';
 import { App } from './app.entity';
 import { AppService } from './app.service';
-import { ContactFormResponse, ContactFormInput } from './dto';
+import { ContactFormInput } from './dto';
 
 @Resolver()
 export class AppResolver {
@@ -18,7 +18,7 @@ export class AppResolver {
     return this.appService.getServerInfo();
   }
 
-  @Mutation(() => ContactFormResponse)
+  @Mutation(() => Boolean)
   contactForm(@Args('detail') _contactForm: ContactFormInput) {
     this.logger.log(
       `Receiving request to contact us with email ${_contactForm.email}...`,
