d0ec3d1cde1dd1033f5a40586a95cc273b9a962e
Miao Zhao <miao_zhao@yahoo.com>
2022-07-25 18:58:51 +1000

    Include AxiosModule in `user.module`

--- src/resource/user/user.module.ts
+++ src/resource/user/user.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { PrismaModule } from '@common';
+import { PrismaModule, AxiosModule } from '@common';
 import { UserService } from './user.service';
 import { UserResolver } from './user.resolver';
 
 @Module({
-  imports: [PrismaModule, ConfigModule],
+  imports: [PrismaModule, ConfigModule, AxiosModule],
   providers: [UserResolver, UserService],
   exports: [UserService],
 })
