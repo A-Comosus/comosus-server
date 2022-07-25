f2b604e7d8f9159a4cfd9f6cb25eafab793ded4d
Miao Zhao <miao_zhao@yahoo.com>
2022-07-25 17:01:57 +1000

    Create `verify-email.input` dto

--- src/resource/auth/dto/index.ts
+++ src/resource/auth/dto/index.ts
@@ -4,3 +4,4 @@ export { RegisterDetailInput } from './register-detail.input';
 export { RegisterResponse } from './register-response';
 export { ForgetPasswordInput } from './forget-password.input';
 export { ResetPasswordInput } from './reset-password.input';
+export { VerifyEmailInput } from './verify-email.input';
