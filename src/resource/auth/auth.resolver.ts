c80c1f2b60a07f4cc537836ba7a3613af7651511
Miao Zhao <miao_zhao@yahoo.com>
2022-07-25 17:10:02 +1000

    Add `verifyUserEmail` into `auth.resolver`

--- src/resource/auth/auth.resolver.ts
+++ src/resource/auth/auth.resolver.ts
@@ -10,6 +10,7 @@ import {
   RegisterDetailInput,
   ForgetPasswordInput,
   ResetPasswordInput,
+  VerifyEmailInput,
 } from './dto';
 
 @Resolver()
@@ -32,6 +33,11 @@ export class AuthResolver {
       `Receiving request to register with email ${_registerDetail.email}...`,
     );
     return this.authService.register(_registerDetail);
+  }
+
+  @Mutation(() => Boolean)
+  verifyUserEmail(@Args('detail') verifyEmailInput: VerifyEmailInput) {
+    return this.authService.verifyUserEmail(verifyEmailInput);
   }
 
   @Mutation(() => Boolean)
