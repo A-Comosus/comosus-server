a3bc93ed31dc6df57892153963a523b5af8674dd
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2022-08-19 12:37:10 +1000

    Remove auth on server info endpoint

--- src/resource/app/app.resolver.ts
+++ src/resource/app/app.resolver.ts
@@ -1,7 +1,6 @@
-import { Logger, UseGuards } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
 import { Query, Resolver, Args, Mutation } from '@nestjs/graphql';
 
-import { JwtAuthGuard } from '@resource/auth/guards';
 import { App } from './app.entity';
 import { AppService } from './app.service';
 import { ContactFormInput } from './dto';
@@ -12,7 +11,6 @@ export class AppResolver {
   constructor(private readonly appService: AppService) {}
 
   @Query(() => App, { name: 'server' })
-  @UseGuards(JwtAuthGuard)
   getServerInfo() {
     this.logger.log('Receiving request for server info...');
     return this.appService.getServerInfo();
